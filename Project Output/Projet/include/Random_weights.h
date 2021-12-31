7712236e34e17df9941ca5515afb892f434b10e1
aicha <aichamaaoui1>
2021-12-31 14:52:54 +0100

    Add Random_weights class
    In this commit, I added the Random_weights class in order to intialize the random weights of the filters.

---
+++ Project Output/Projet/include/Random_weights.h
@@ -0,0 +1,12 @@
+#ifndef __RANDOM_WEIGHTS_H__
+#define __RANDOM_WEIGHTS_H__
+
+#include <iostream>
+#include <vector> //vector
+#include <chrono> //time
+#include <cmath> //math
+#include <random> //random
+
+void Random_weights(double nb_filters, double nb_weights, std::vector<std::vector<double>>& nb_tot);
+
+#endif
