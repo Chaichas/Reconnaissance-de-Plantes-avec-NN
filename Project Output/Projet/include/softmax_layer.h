c6217ab11aa9267811def02250b8d05273579aeb
Hmina97 <80129847+Hmina97@users.noreply.github.com>
2022-01-02 00:50:40 +0100

    update header class softmax

--- Project Output/Projet/include/softmax_layer.h
+++ Project Output/Projet/include/softmax_layer.h
@@ -5,8 +5,11 @@
 #include <vector>
 
 //Macros pour la couche softmax
-#define ND 10
+#define ND 5
 #define DEPTH 8
+
+
+
 
 
 class Softmax {
@@ -16,14 +19,23 @@ public:
 	~Softmax();  //destructeur
 
 	std::vector<double> Softmax_start(const std::vector<std::vector<double>>& img_input, int img_height, int img_width);
-
+    
 	
 private:
-
-
+    
+	void Flatten_making(const std::vector<std::vector<double>>& img_input, int d);
+    
 	int mLength; 
 
-	std::vector<std::vector<double>> mWeight;    // vecteur de poids 
+	std::vector<std::vector<double>> mWeights;    // vecteur de poids 
+	
+	std::vector<double> mBiases; 	//couche vecteur de biais 
+
+	std::vector<double> mFlatten; //Vecteur pour stocker l'entrée en tant que vecteur d'aplatissement
+	
+	std::vector<double> mTotal; //Vecteur de stockage des prédictions finales pour chaque nœud (chiffre)
+
+  
 
 
 	
