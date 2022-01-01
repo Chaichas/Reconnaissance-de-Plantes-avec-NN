e04ab76febbf910d4324d2bdc6cff2a7acf59a91
Abdel-BHPC <91852442+Abdel-BHPC@users.noreply.github.com>
2022-01-01 21:36:50 +0100

    Add Training_data function in class Output

---
+++ Project Output/Projet/source/output.c
@@ -0,0 +1,37 @@
+
+void Output::Training_data(int numb_epoch, double alpha)
+{
+  std::cout << "--------------Start Training ----------" << '\n';
+  
+  std::vector<int> Labels;
+  std::vector<std::string> training_files = Reader::Process_directory(m_trainPath, Labels);
+
+  int it=0;
+  while(it < numb_epoch) {
+
+    int label_i = 0;
+    double runningAcc = 0.0, runningLoss = 0.0;
+
+    for (std::vector<std::string>::iterator it = trainingFiles.begin() ; it != trainingFiles.end(); ++it)
+    {
+      std::string name = *it;
+      int hauteur = 0, largeur = 0;
+      
+      m_image->loadImage(name, hauteur, largeur);
+      label_i = std::distance(trainingFiles.begin(), it);
+           
+      //Lancement de training
+      //train(    parametres               );
+
+      runningLoss += loss;
+      runningAcc += acc;
+    }
+    
+    label_i++;
+    //Affichage de perte et de la precison pour chaque epoch 
+    std::cout << "Epoch " << it << " : Average Loss " << runningLoss / label_i << " , Accuracy " << (runningAcc/label_i)*100 << " %" << '\n';
+    runningLoss = 0.0;
+    runningAcc = 0.0;
+    it++;
+  }
+}
