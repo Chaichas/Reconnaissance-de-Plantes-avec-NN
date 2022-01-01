fe2f8e4e1220566061795db78398b48303bd138a
Abdel-BHPC <91852442+Abdel-BHPC@users.noreply.github.com>
2022-01-01 19:48:34 +0100

    Add class Output header

---
+++ Project Output/Projet/include/Output.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <vector>
+#include "Data.h"
+#include "convolution_layer.h"
+#include "pooling.h"
+#include "softmax.h"
+
+class Output
+{
+
+private:
+  //Dataset pour le training et le test
+  std::string file_train;
+  std::string file_test;
+
+  Data * m_image;
+  Convolution_layer * m_convol;
+  Pooling * m_pool;
+  Softmax * m_softmax;
+  void train(int label, int& hauteur, int& largeur, double& lRate);
+
+public:
+  output(std::string file_train, std::string file_test);
+  ~output();
+
+  void Training_data(int trainEpoch = 1, double alpha = 0.005);
+
+};
+
+
