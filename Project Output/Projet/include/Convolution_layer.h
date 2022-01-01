570ce87e55bb3108aa4c8ebd95871e7cd6948295
aicha <aichamaaoui1>
2022-01-01 18:50:05 +0100

    Add convolution_process method to Convolution_layer class
    In this commit, I added the convolution_process method, performing the multiplication and summation of matrix pixel and filter values.

--- Project Output/Projet/include/Convolution_layer.h
+++ Project Output/Projet/include/Convolution_layer.h
@@ -10,11 +10,22 @@ public:
 	Convolution_layer(); //Constructor
 	~Convolution_layer(); //Destructor
 
-	void convolution_parameters(const std::vector<double>& kernel, int inputImage_height, int inputImage_width);
+	void convolution_parameters(const std::vector<double>& pixel_vec, int inputImage_height, int inputImage_width);
 
 	 //Get Height and Width of the convolutional output matrix
 	int getMatHeight() {return ConvMat_height;} //Convolutional output matrix height
 	int getMatWidth() {return ConvMat_width;} //Convolutional output matrix width
+	
+	//Get Convolution output Matrix
+	const std::vector<std::vector<double>>& getConvMat() const {return ConvMat;} 
+	
+private
+
+	std::vector<std::vector<double>> ConvMat; //convolution matrix filled with values
+	
+	std::vector<std::vector<double>> filter_matrix; //Filter matrix filled with values
+	
+	void convolution_process(const std::vector<double>& pixel, int idx); //multiplication and summation pixel value * filter value
 
 }
 
--- Project Output/Projet/source/Convolution_layer.cpp
+++ Project Output/Projet/source/Convolution_layer.cpp
@@ -12,29 +12,65 @@
 Convolution_layer::Convolution_layer(){} //constructor
 
 
-
-void Convolution_layer::convolution_parameters(const std::vector<double>& kernel, int inputImage_height, int inputImage_width){
+void Convolution_layer::convolution_parameters(const std::vector<double>& pixel_vec, int inputImage_height, int inputImage_width){
 
 	bool initialization = true; //initializing filters with random values
 	
-	ConvMat_height = ((inputImage_height - filter_height +2 *padding)/stride )+1; //output convolution matrix height
-	ConvMat_width = ((inputImage_width - filter_width +2 *padding)/stride )+1; //output convolution matrix width
+	int ConvMat_height = ((inputImage_height - filter_height +2 *padding)/stride )+1; //output convolution matrix height
+	int ConvMat_width = ((inputImage_width - filter_width +2 *padding)/stride )+1; //output convolution matrix width
 	
 	//initialization of the filter weights by random values (class Random_weights)
 	if (initialization){
 		Random_weights(nb_filters, filter_height*filter_width, filter_matrix); //initialization of weights with random values
 		
 		//normalizing wight values, Ref3
-		for (size_t ii=0; ii < filter_number; ii++){ //loop on number of filters
-			for (size_t jj=0; jj < (filter_height*filter_width); jj++){ //loop on total number of weights within each filter
+		for (size_t ii = 0; ii < filter_number; ii++){ //loop on number of filters
+			for (size_t jj = 0; jj < (filter_height*filter_width); jj++){ //loop on total number of weights within each filter
 				filter_matrix[ii][jj] = (double) filter_matrix[ii][jj]/(double) (filter_height*filter_width); //normalized filter
 			}
 		}
 		initialization = false;
 	}
 	
-}
+	ConvMat.clear();
 	
+	//Convolution procedure for filter_number
+	convolution_process(pixel_vec, 0); //1st filter
+	convolution_process(pixel_vec, 1); //2sd filter
+	convolution_process(pixel_vec, 2); //3rd filter
+	convolution_process(pixel_vec, 3); //4th filter
+	convolution_process(pixel_vec, 4); //5th filter
+	convolution_process(pixel_vec, 5); //6th filter
+	convolution_process(pixel_vec, 6); //7th filter
+	convolution_process(pixel_vec, 7); //8th filter
+}
+
+//Convolution Process
+void Convolution_layer::convolution_process(const std::vector<double>& pixel, int idx){
+	std::vector<double> vec;
+	
+	for (int ii = 0; ii < ConvMat_height; ii++){ //loop on the height of the convolution matrix
+		for (int jj = 0; jj < ConvMat_width; jj++){ //loop on the width of the convolution matrix
+			
+			double sum = 0; //initialization of the summation
+			
+				for (int kk=0; kk < filter_height; kk++){ //loop on the height of the filter
+					for (int hh=0; hh < filter_width; hh++){ //loop on the width of the filter
+					
+						double Temp = (pixel[((ii + kk) * ((ConvMat_width - 1) * stride + filter_width - 2*padding) + (jj + hh))]); //pixel value of the image stored in Temp
+						
+						sum = sum + Temp * filter_matrix[idx][kk * filter_width + hh]; //sum of the pixel value * filter value
+					
+					}				
+				}
+				vec.push_back(sum); //adding sum value to vec		
+		}		
+	}
+	ConvMat.push_back(vec); //adding vec value to ConvMat
+}
+
+
+Convolution_layer::~Convolution_layer(){} //Destructor
 
 
 
@@ -43,3 +79,4 @@ void Convolution_layer::convolution_parameters(const std::vector<double>& kernel
 Ref3: https://en.wikipedia.org/wiki/Kernel_(image_processing)
 
 */
+
