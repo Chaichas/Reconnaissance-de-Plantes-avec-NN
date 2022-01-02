cd87e0bfecf6c9f51fc57f2564571cb97bd7656b
aicha <aichamaaoui1>
2022-01-02 14:24:24 +0100

    Add the Pooling_parameters method in Pooling_layer class
    In this commit, I added the Pooling_parameters method to the Pooling_layer class, where the ouptut height and width of the pooling output matrix is calculated.

--- Project Output/Projet/include/Pooling_layer.h
+++ Project Output/Projet/include/Pooling_layer.h
@@ -5,6 +5,15 @@
 #include <vector> //vector
 
 
+class Pooling_layer{
 
+public:
+
+	Pooling_layer(); //Constructor
+	~Pooling_layer(); //Destructor
+	
+	void Pooling_parameters(const std::vector<double>& vec_convolution, int input_height, int input_width);
+	
+};
 
 #endif
--- Project Output/Projet/source/Pooling_layer.cpp
+++ Project Output/Projet/source/Pooling_layer.cpp
@@ -1,1 +1,16 @@
 #include "Pooling_layer.h"
+#define Pooling_size 2
+
+Pooling_layer::Pooling_layer(){} //constructor
+
+
+void Pooling_layer::Pooling_parameters(const std::vector<double>& vec_convolution, int input_height, int input_width){
+
+	int Pooling_height = input_height/Pooling_size; //Height of the output pooling matrix
+	int Pooling_width = input_width/Pooling_size; //Width of the output pooling matrix
+	
+}
+
+
+
+Pooling_layer::~Pooling_layer(){} //Destructor
